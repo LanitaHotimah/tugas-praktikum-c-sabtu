c1b534ead0c4f2783c3dcd74347335baed414673
LanitaHotimah <lanitahotimah0321@gmail.com>
2024-09-27 00:02:19 +0700

    Add files via upload

---
+++ Lanita 1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    char nim[] = "2441022"; 
+    char nama[] = "Lanita Hotimah"; 
+    char jurusan[] = "Teknik Informatika"; 
+
+    printf("NIM: %s\n", nim);
+    printf("NAMA: %s\n", nama);
+    printf("JURUSAN: %s\n", jurusan);
+
+    return 0;
+}
+
